17a68b30b5a51784cf990c57ed6a7e175340e07b
LCA-0907 <50255697+LCA-0907@users.noreply.github.com>
2020-05-07 23:07:30 +0800

    Update hw3.py
    add my own BFmatch function

--- Lab3/hw3.py
+++ Lab3/hw3.py
@@ -1,9 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import math
 
-imname1 = 'data/1.jpg'
-imname2 = 'data/2.jpg'
+class myMatch():
+    def __init__(self, id, distance):
+        self.id = id
+        self.distance = distance
+
+def BFmatch (des1, des2, k):
+    match = []
+    idx1 = 0
+    for p1 in des1:
+        best_m = []
+        temp0 = cv2.DMatch(idx1, 0, math.sqrt((p1 - des2[0]).T.dot(p1 - des2[0])))
+        temp1 = cv2.DMatch(idx1, 1, math.sqrt((p1 - des2[1]).T.dot(p1 - des2[1])))
+        if temp0.distance < temp1.distance:
+            best_m.append(temp0)
+            best_m.append(temp1)       
+        else:
+            best_m.append(temp1)
+            best_m.append(temp0)
+            
+        idx2 = 0
+        for p2 in des2:
+            dis = math.sqrt((p1-p2).T.dot((p1-p2)))
+            if dis < best_m[0].distance:
+                best_m[0].trainIdx = idx2
+                best_m[0].distance = dis
+            elif dis < best_m[1].distance:
+                best_m[1].trainIdx = idx2
+                best_m[1].distance = dis
+            idx2 = idx2 + 1
+        idx1 = idx1 + 1
+        match.append(best_m)
+    return match
+
+
+imname1 = 'data/S1.jpg'
+imname2 = 'data/S2.jpg'
 
 # part1 
 img1 = cv2.imread(imname1)
@@ -16,33 +51,41 @@ kp2, des2 = sift.detectAndCompute(Gimg2, None)
 
 # part2
 
-#找match不確定可不可以用cv2 function
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
+#找match不確定可不可以用cv2 function 好像 不行(?)
+#bf = cv2.BFMatcher()
+#matches = bf.knnMatch(des1, des2, k=2)
+Mymatches = BFmatch(des1, des2, 2)
 
 # Apply ratio test
-good = []
+'''good = []
 for m in matches:
-    if m[0].distance < 0.7*m[1].distance:
+    if m[0].distance < 0.8*m[1].distance:
         good.append((m[0].trainIdx, m[0].queryIdx))
 matches = np.asarray(good)
-
+'''
+temp = []
+for m in Mymatches:
+    if m[0].distance < 0.8*m[1].distance:
+        temp.append((m[0].trainIdx, m[0].queryIdx))
+Mymatches = np.asarray(temp)
+  
+# 畫圖
 (hA, wA) = img1.shape[:2]
 (hB, wB) = img2.shape[:2]
 vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
 vis[0:hA, 0:wA] = img1
 vis[0:hB, wA:] = img2
-
-for (trainIdx, queryIdx) in matches:
+ 
+for (trainIdx, queryIdx) in Mymatches:
     temp = np.random.randint(0, high=255, size=(3,))
     color = (np.asscalar(temp[0]), np.asscalar(temp[1]), np.asscalar(temp[2]))
     #print(color)
     ptA = (int(kp1[queryIdx].pt[0]), int(kp1[queryIdx].pt[1]))
     ptB = (int(kp2[trainIdx].pt[0] + wA), int(kp2[trainIdx].pt[1]))
-
     cv2.line(vis, ptA, ptB, color, 1)
 plt.imshow(vis)
 plt.show()
+
 
 # part3 
 #do RANSAC
@@ -50,10 +93,6 @@ n_times = 100
 s = 4 #pick 4 points to find H
 for it in range(n_times):
     sample_idx = np.random.choice(matches.size, s, replace=False)
-    print(sample_idx)
     # find H
     H = np.zeros(9)
-    
-    
-    
     
