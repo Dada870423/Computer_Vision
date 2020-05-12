4e100586363028dc7d7855b274ba9b9db6e81a0a
LCA-0907 <50255697+LCA-0907@users.noreply.github.com>
2020-05-12 17:03:26 +0800

    change BF match into class

--- Lab3/hw3.py
+++ Lab3/hw3.py
@@ -4,38 +4,10 @@ import cv2
 import math
 from RANSAC import *
 from WARP import *
+from BFMATCH import *
 
-def BFmatch (des1, des2, k):
-    match = []
-    idx1 = 0
-    for p1 in des1:
-        best_m = []
-        temp0 = cv2.DMatch(idx1, 0, math.sqrt((p1 - des2[0]).T.dot(p1 - des2[0])))
-        temp1 = cv2.DMatch(idx1, 1, math.sqrt((p1 - des2[1]).T.dot(p1 - des2[1])))
-        if temp0.distance < temp1.distance:
-            best_m.append(temp0)
-            best_m.append(temp1)       
-        else:
-            best_m.append(temp1)
-            best_m.append(temp0)
-            
-        idx2 = 0
-        for p2 in des2:
-            dis = math.sqrt((p1-p2).T.dot((p1-p2)))
-            if dis < best_m[0].distance:
-                best_m[0].trainIdx = idx2
-                best_m[0].distance = dis
-            elif dis < best_m[1].distance:
-                best_m[1].trainIdx = idx2
-                best_m[1].distance = dis
-            idx2 = idx2 + 1
-        idx1 = idx1 + 1
-        match.append(best_m)
-    return match
-
-
-imname1 = 'data/S2.jpg'
-imname2 = 'data/S1.jpg'
+imname1 = 'data/2.jpg'
+imname2 = 'data/1.jpg'
 
 # part1 
 img1 = cv2.imread(imname1)
@@ -50,7 +22,8 @@ kp2, des2 = sift.detectAndCompute(Gimg2, None)
 
 #bf = cv2.BFMatcher()
 #matches = bf.knnMatch(des1, des2, k=2)
-Mymatches = BFmatch(des1, des2, 2)
+BFmatch = BFMATCH()
+Mymatches = BFmatch.Best2Matches(des1, des2)
 
 # Apply ratio test
 '''good = []
@@ -66,8 +39,6 @@ for m in Mymatches:
         temp.append((m[0].trainIdx, m[0].queryIdx))
         Mm.append(m[0])
 Mymatches = np.asarray(temp)
-
-
 
 
 Mm = sorted(Mm, key=lambda x: x.distance)
@@ -91,7 +62,7 @@ for (trainIdx, queryIdx) in Mymatches:
     #print(color)
     ptA = (int(kp1[queryIdx].pt[0]), int(kp1[queryIdx].pt[1]))
     ptB = (int(kp2[trainIdx].pt[0] + wA), int(kp2[trainIdx].pt[1]))
-    cv2.line(vis, ptA, ptB, color, 1)
+    #cv2.line(vis, ptA, ptB, color, 1)
     (x1, y1) = (kp1[queryIdx].pt)
     (x2, y2) = (kp2[trainIdx].pt)
     CorList.append([x1, y1, x2, y2])
@@ -140,8 +111,6 @@ cv2.imshow("Keypoint Matches of image", Match_picture)
 cv2.imshow("Result of merged image", ResultImg)
 
 ## cv2.waitKey(0)
-
-
 
 plt.imshow(ResultImg)
 plt.show()
