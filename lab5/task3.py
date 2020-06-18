23060d87c53385a609e1ec775f0dfa45443b9b4e
LCA-0907 <50255697+LCA-0907@users.noreply.github.com>
2020-06-18 16:44:14 +0800

    add scaled files

--- lab5/task3.py
+++ lab5/task3.py
@@ -3,123 +3,91 @@ from libsvm.svm import *
 import random
 import numpy as np
 from sklearn.svm import SVC
+import sys
 
-# train 讀第一次取得max, min
-"""
-filename = 'train20.txt'
-data_max = 0
-data_min = 0
-with open(filename, 'r') as file_to_read:
-    while True:
-        lines = file_to_read.readline() # 整行讀取資料
-        if not lines:
-            break
-        a = lines.split()
-        t1 = float(a[1][2:])
-        t2 = float(a[2][2:])
-        t3 = float(a[3][2:])
-        if max(t1,t2,t3) > data_max :
-            data_max = max(t1, t2, t3)
-        if min(t1,t2,t3) < data_min :
-            data_min = min(t1, t2, t3)
+n_cluster=80
+
 # 讀第二次存成tr_histogram
 tr_histogram = []
-filename = 'train20.txt'
+filename = 'train/train80.txt'
 with open(filename, 'r') as file_to_read:
     while True:
         lines = file_to_read.readline() # 整行讀取資料
         if not lines:
             break
         a = lines.split()
-        t_sum =  float(a[1][2:]) + float(a[2][2:]) +  float(a[3][2:])
+        t_sum = 0
+        for i in range(1, len(a)):
+            colon = a[i].split(":")
+            t_sum += float(colon[1])
         # normalize
-        t1 = float(a[1][2:]) / t_sum
-        t2 = float(a[2][2:]) / t_sum
-        t3 = float(a[3][2:]) / t_sum
-        tr_histogram.append(([t1, t2, t3], int(a[0])))
+        tt = np.zeros(len(a) - 1)
+        for i in range(1, len(a)):
+            colon = a[i].split(":")
+            tt[i-1] = float(colon[1]) / t_sum
+        tr_histogram.append((tt, int(a[0])))
 
-# test 讀第一次取得max, min
-filename = 'test20.txt'
-data_max = 0
-data_min = 0
-with open(filename, 'r') as file_to_read:
-    while True:
-        lines = file_to_read.readline() # 整行讀取資料
-        if not lines:
-            break
-        a = lines.split()
-        t1 = float(a[1][2:])
-        t2 = float(a[2][2:])
-        t3 = float(a[3][2:])
-        if max(t1,t2,t3) > data_max :
-            data_max = max(t1, t2, t3)
-        if min(t1,t2,t3) < data_min :
-            data_min = min(t1, t2, t3)
-            # 讀第二次存成tr_histogram
+
+# 讀第二次存成tr_histogram
 te_histogram = []
-filename = 'test20.txt'
+filename = 'test/test80.txt'
 with open(filename, 'r') as file_to_read:
     while True:
         lines = file_to_read.readline() # 整行讀取資料
         if not lines:
             break
         a = lines.split()
+        t_sum = 0
+        for i in range(1, len(a)):
+            colon = a[i].split(":")
+            t_sum += float(colon[1])
         # normalize
-        t_sum =  float(a[1][2:]) + float(a[2][2:]) +  float(a[3][2:])
-        t1 = float(a[1][2:]) / t_sum
-        t2 = float(a[2][2:]) / t_sum
-        t3 = float(a[3][2:]) / t_sum
-        te_histogram.append(([t1, t2, t3], int(a[0])))
+        tt = np.zeros(len(a) - 1)
+        for i in range(1, len(a)):
+            colon = a[i].split(":")
+            tt[i-1] = float(colon[1]) / t_sum
+        te_histogram.append((tt, int(a[0])))
 
-'''
+
 # write train data
 seq = np.arange(0, len(tr_histogram))
-np.random.shuffle(seq)
-data = open("tr2.txt",'w+')
+data = open("train/train80_add_1.txt",'w+')
 for i in seq:
-    s = str(tr_histogram[i][1])+' 1:'+str(tr_histogram[i][0][0])+' 2:'+str(tr_histogram[i][0][1])+' 3:'+str(tr_histogram[i][0][2])
-    print(s,file=data)
+    s = str(tr_histogram[i][1])
+    for j in range(n_cluster):
+        s = s +" " + str(j+1) + ":" + str(tr_histogram[i][0][j])
+    print(s, file=data)
 data.close()
+
+
 
 # write test data
 seq = np.arange(0, len(te_histogram))
-np.random.shuffle(seq)
-data = open("te2.txt",'w+')
+data = open("test/test80_add_1.txt",'w+')
 for i in seq:
-    s = str(te_histogram[i][1])+' 1:'+str(te_histogram[i][0][0])+' 2:'+str(te_histogram[i][0][1])+' 3:'+str(te_histogram[i][0][2])
-    print(s,file=data)
+    s = str(te_histogram[i][1])
+    for j in range(n_cluster):
+        s = s +" " + str(j+1) + ":" + str(te_histogram[i][0][j])
+    print(s, file=data)
 data.close()
-'''
-"""
+
+
 # use libsvm
-"""
-train_x = []
-train_y = []
-test_x = []
-test_y = []
-for temp in tr_histogram:
-    train_x.append(temp[0])
-    train_y.append(temp[1])
-    print(temp)
-for temp in te_histogram:
-    test_x.append(temp[0])
-    test_y.append(temp[1])
-"""
-y, x = svm_read_problem('train/train20_scale.txt')
-yt, xt = svm_read_problem('test/test20_scale.txt')
+y, x = svm_read_problem('train/train80_add_1.txt')
+yt, xt = svm_read_problem('test/test80_add_1.txt')
 
 #clf = SVC(kernel = 'linear', probability = True)
 #clf.fit(train_x, train_y)
 #print(clf.score(test_x, test_y))
 C = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
-gamma = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
+gamma = [1e-6, 1e-5, 1e-4, 0.001, 0.01, 0.1, 1]
 
-model = svm_train(y, x, '-t 0')
-p_label, p_acc, p_val = svm_predict(yt, xt, model)
-print('test:')
-print(p_label)
+#model = svm_train(y, x, '-t 0')
+#p_label, p_acc, p_val = svm_predict(yt, xt, model)
+#print('test:')
+#print(p_label)
 
-"""
+
 best_acc = 0
 best_para = (0, 0)
 for cidx in C:
@@ -130,9 +98,8 @@ for cidx in C:
         p_label, p_acc, p_val = svm_predict(yt, xt, model)
         print('test:')
         print(p_label)
-        if max(p_acc) > best_acc:
+        if max(p_acc) >= best_acc:
             best_acc = max(p_acc)
             best_para = (cidx, gidx)
 print("best_acc:", best_acc)
 print("best_para:", best_para)
-"""
