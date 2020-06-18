551168d67fa2264e9036aeaf5ae7f71e8070a400
dada <dada0423.cs05@g2.nctu.edu.tw>
2020-06-18 17:45:02 +0800

    to svm with test

--- lab5/predict/tosvm.py
+++ lab5/predict/tosvm.py
@@ -4,8 +4,8 @@ import cv2
 import math
 import random
 
-train_path = "./train/train50.txt"
-test_path = "./test/test50.txt"
+train_path = "../train/train50.txt"
+test_path = "../test/test50.txt"
 
 n_cluster = 51
 
@@ -25,7 +25,7 @@ with open(train_path, "r") as file_to_read:
             train_his.append((x, int(row[0])))
 #print(train_his)
 feature = open("filetrain50.txt",'w+')
-class_file = open("fileclass.txt",'w+')
+class_file = open("filetrainclass.txt",'w+')
 for (row, Class_) in train_his:
     s = ""
     for j in range(len(row)):
@@ -35,8 +35,31 @@ for (row, Class_) in train_his:
 feature.close()
 class_file.close()
 
-
-
+## test
+test_his = []
+with open(train_path, "r") as file_to_read:
+    while True:
+        lines = file_to_read.readline() # 整行讀取資料
+        if not lines:
+            break
+        row = lines.split()
+        # normalize
+        x = list()
+        for i in range(1, n_cluster):
+            colon = row[i].split(":")
+            x.append(float(colon[1]))
+            test_his.append((x, int(row[0])))
+#print(test_his)
+feature = open("filetest50.txt",'w+')
+class_file = open("filetestclass.txt",'w+')
+for (row, Class_) in test_his:
+    s = ""
+    for j in range(len(row)):
+        s = s + " " + str(row[j])
+    print(s, file = feature)
+    print(Class_, file = class_file)
+feature.close()
+class_file.close()
 
 
 
